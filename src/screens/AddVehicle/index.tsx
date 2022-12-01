17039c3df5aa9a4fbc8dc9377986a6d59e8f99cb
Marcos Souza Brito <njsu5@hotmail.com>
2022-12-01 14:51:22 -0300

    primeira versão dos dados dos veiculos

--- src/screens/AddVehicle/index.tsx
+++ src/screens/AddVehicle/index.tsx
@@ -4,9 +4,45 @@ import OptionsScreens from '../../components/OptionsScreens'
 import { Button, Container, Name, SingOut, SingOutIcon, Text } from './styled'
 import firestore from '@react-native-firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import auth from '@react-native-firebase/auth';
 
 const AddVehicle = () => {
+
+  const vehicle = {
+    specifications: {
+      'Engine Type': '',
+      'No. of cylinder': '',
+      'Fuel Type': '',
+      'Max Torque': '',
+      'Transmission Type': '',
+      'Gear Box': '',
+      'Drive Type': '',
+      'Fuel Tank Capacity': '',
+      'Max Power': '',
+      'Seating Capacity': '',
+      'Boot Space (Litres)': '',
+      'Body Type': '',
+    },
+    features: {
+      'Power Steering': false,
+      'Air Conditioner': false,
+      'Anti Lock Braking System': false,
+      'Passenger Airbag': false,
+      'Fog Lights - Front': false,
+      'Power Windows Front': false,
+      'Wheel Covers': false,
+      'Driver Airbag': false,
+      'Automatic Climate Control': false
+    },
+    description: {
+      Make: '',
+      Model: '',
+      Variant: '',
+      Color: '',
+      Odometer: '',
+      Year: '',
+
+    }
+  }
 
 
   return (
