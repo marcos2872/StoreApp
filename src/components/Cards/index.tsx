aee83e20c95bcfaff5763363abc29c19f9ed7052
Marcos Souza Brito <njsu5@hotmail.com>
2022-12-03 01:25:52 -0300

    buscando veiculos

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import firestore from '@react-native-firebase/firestore';
+import storage, { FirebaseStorageTypes } from '@react-native-firebase/storage';
+
 import { Card, Container, Image, Price, Scroll, Text, Title } from './styled'
 
 const Cards = () => {
+  const [vehiclesData, setVehiclesData] = useState<any>([])
+
+  useEffect(() => {
+
+    const teste = async () => {
+      const data: any = []
+      const snapshot = await firestore().collection('vehicles').get()
+        snapshot.forEach(doc => {
+          data.push({id: doc.id, ...doc.data()})
+        })
+
+      const allVehicles = await Promise.all(data.map(async (curr: any) => {
+        const list = await storage().ref(`${curr.description.Model}vehicles,${curr.id}`).list()
+        const urls = await Promise.all(list.items.map(async (ref: { fullPath: any; }) => {
+              const url = await storage().ref(ref.fullPath).getDownloadURL()
+                return url;
+              }));
+            return {...curr, images: urls}
+          }))
+          setVehiclesData(allVehicles) 
+    }
+        teste()
+  }, [])
+
   return (
     <Container>
-      <Scroll>
-        
+      <Scroll> 
       <Card>
         <Image />
         <Title>Title</Title>
